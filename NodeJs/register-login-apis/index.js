ccb6ae83f6661cbb17ed59f9b50cfdaa560c7564
Ganesh Kumar <bojjaganeshkumar1@gmail.com>
2023-12-02 11:59:30 +0530

    added authenticateToken middleware fun to all APIs

--- NodeJs/register-login-apis/index.js
+++ NodeJs/register-login-apis/index.js
@@ -63,13 +63,7 @@ app.post("/login/", async (request, response) => {
   }
 });
 
-//Get Books API
-app.get("/books/", async (request, response) => {
-  const {
-    search_q = "",
-    sort_by = "book_id",
-    sort_order = "ASC",
-  } = request.query;
+const authenticateToken = async (request, response, next) => {
   let jwtToken;
   const { authorization } = request.headers;
   if (authorization !== undefined) {
@@ -81,119 +75,133 @@ app.get("/books/", async (request, response) => {
         response.status(401);
         response.send("Invalid Access Token");
       } else {
-        const getBooksQuery = `SELECT * FROM books WHERE title LIKE '%${search_q}%' ORDER BY ${sort_by} ${sort_order};`;
-        const books = await db.all(getBooksQuery);
-        response.send(books);
+        next();
       }
     });
   }
+};
+
+//Get Books API
+app.get("/books/", authenticateToken, async (request, response) => {
+  const {
+    search_q = "",
+    sort_by = "book_id",
+    sort_order = "ASC",
+  } = request.query;
+  const getBooksQuery = `SELECT * FROM books WHERE title LIKE '%${search_q}%' ORDER BY ${sort_by} ${sort_order};`;
+  const books = await db.all(getBooksQuery);
+  response.send(books);
 });
 
-// // Get specific book API
-// app.get("/books/:bookId", async (request, response) => {
-//   const { bookId } = request.params;
-//   const getBookQuery = `
-//     SELECT * FROM books WHERE book_id=${bookId};`;
-//   const bookDetails = await db.get(getBookQuery);
-//   response.send(bookDetails);
-// });
+// Get specific book API
+app.get("/books/:bookId", authenticateToken, async (request, response) => {
+  const { bookId } = request.params;
+  const getBookQuery = `
+    SELECT * FROM books WHERE book_id=${bookId};`;
+  const bookDetails = await db.get(getBookQuery);
+  response.send(bookDetails);
+});
 
-// //Add Book API
-// app.post("/books/", async (request, response) => {
-//   const bookDetails = request.body;
-//   const {
-//     title,
-//     authorId,
-//     rating,
-//     ratingCount,
-//     reviewCount,
-//     description,
-//     pages,
-//     dateOfPublication,
-//     editionLanguage,
-//     price,
-//     onlineStores,
-//   } = bookDetails;
-//   const addBookQuery = `
-//     INSERT INTO
-//       books (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
-//     VALUES
-//       (
-//         '${title}',
-//          ${authorId},
-//          ${rating},
-//          ${ratingCount},
-//          ${reviewCount},
-//         '${description}',
-//          ${pages},
-//         '${dateOfPublication}',
-//         '${editionLanguage}',
-//          ${price},
-//         '${onlineStores}'
-//       );`;
+//Add Book API
+app.post("/books/", authenticateToken, async (request, response) => {
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+  const addBookQuery = `
+    INSERT INTO
+      books (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`;
 
-//   const dbResponse = await db.run(addBookQuery);
-//   const bookId = dbResponse.lastID;
-//   response.send({ bookId: bookId });
-// });
+  const dbResponse = await db.run(addBookQuery);
+  const bookId = dbResponse.lastID;
+  response.send({ bookId: bookId });
+});
 
-// // Update Book API
-// app.put("/books/:bookId/", async (request, response) => {
-//   const { bookId } = request.params;
-//   const bookDetails = request.body;
-//   const {
-//     title,
-//     authorId,
-//     rating,
-//     ratingCount,
-//     reviewCount,
-//     description,
-//     pages,
-//     dateOfPublication,
-//     editionLanguage,
-//     price,
-//     onlineStores,
-//   } = bookDetails;
-//   const updateBookQuery = `
-//       UPDATE
-//         books
-//       SET
-//         title='${title}',
-//         author_id=${authorId},
-//         rating=${rating},
-//         rating_count=${ratingCount},
-//         review_count=${reviewCount},
-//         description='${description}',
-//         pages=${pages},
-//         date_of_publication='${dateOfPublication}',
-//         edition_language='${editionLanguage}',
-//         price=${price},
-//         online_stores='${onlineStores}'
-//       WHERE
-//         book_id = ${bookId};`;
-//   await db.run(updateBookQuery);
-//   response.send("Book Updated Successfully");
-// });
+// Update Book API
+app.put("/books/:bookId/", authenticateToken, async (request, response) => {
+  const { bookId } = request.params;
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+  const updateBookQuery = `
+      UPDATE
+        books
+      SET
+        title='${title}',
+        author_id=${authorId},
+        rating=${rating},
+        rating_count=${ratingCount},
+        review_count=${reviewCount},
+        description='${description}',
+        pages=${pages},
+        date_of_publication='${dateOfPublication}',
+        edition_language='${editionLanguage}',
+        price=${price},
+        online_stores='${onlineStores}'
+      WHERE
+        book_id = ${bookId};`;
+  await db.run(updateBookQuery);
+  response.send("Book Updated Successfully");
+});
 
-// //Delete Book API
-// app.delete("/books/:bookId/", async (request, response) => {
-//   const { bookId } = request.params;
-//   const deleteBookQuery = `
-//     DELETE FROM books WHERE book_id=${bookId};`;
-//   await db.run(deleteBookQuery);
-//   response.send("Book Deleted Successfully");
-// });
+//Delete Book API
+app.delete("/books/:bookId/", authenticateToken, async (request, response) => {
+  const { bookId } = request.params;
+  const deleteBookQuery = `
+    DELETE FROM books WHERE book_id=${bookId};`;
+  await db.run(deleteBookQuery);
+  response.send("Book Deleted Successfully");
+});
 
-// //Get Author Books API
-// app.get("/authors/:authorId/books/", async (request, response) => {
-//   const { authorId } = request.params;
-//   const getAuthorBooksQuery = `
-//       SELECT
-//        *
-//       FROM
-//        books
-//       WHERE
-//         author_id = ${authorId};`;
-//   const booksArray = await db.all(getAuthorBooksQuery);
-//   response.send(booksArray);
-// });
+//Get Author Books API
+app.get(
+  "/authors/:authorId/books/",
+  authenticateToken,
+  async (request, response) => {
+    const { authorId } = request.params;
+    const getAuthorBooksQuery = `
+      SELECT
+       *
+      FROM
+       books
+      WHERE
+        author_id = ${authorId};`;
+    const booksArray = await db.all(getAuthorBooksQuery);
+    response.send(booksArray);
+  }
+);
